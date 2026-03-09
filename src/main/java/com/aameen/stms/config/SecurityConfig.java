923f985b2e916f8b37f5ec1c200cf543cb2c529d
Aameen <aameen.se@gmail.com>
2026-03-09 15:58:18 +0530

    Allow Swagger API docs

--- src/main/java/com/aameen/stms/config/SecurityConfig.java
+++ src/main/java/com/aameen/stms/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter,
